beeb530742c5073d7a71c93a124dd34b1c3b7b91
Maxime <maxime.marechalmccoy@gmail.com>
2023-10-18 09:33:29 -0700

    partial for details and a few comments

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,9 @@
 class CategoriesController < ApplicationController
 
   def show
+    # raise "Checking something"
     @category = Category.find(params[:id])
+    # raise @category.inspect
     @products = @category.products.order(created_at: :desc)
   end
 
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,16 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find params[:id]
+    # puts "In Product show"
+    
+    # Implicit in Rails. Automatically renders the action result. Goes into the yield of layout/application
+    # render :show #or 'show'
+    # 
+    #If we were to want to render another path, could do with the string such as
+    #render 'shared/show"
+    #
+    #Any other instance variabale will be sent to the view template to use
+    
   end
 
 end
