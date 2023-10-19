1216aef8a823f52baef5bb69ea54e6be33952326
Maxime <maxime.marechalmccoy@gmail.com>
2023-10-19 10:13:55 -0700

    scaffolded controller and views generation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'categories/index'
+    get 'categories/new'
+    get 'categories/create'
+  end
   root to: 'products#index'
   
   resources :about, only: [:index]
@@ -17,6 +22,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:new, :index, :create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
