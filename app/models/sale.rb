964b2d9bc7c1ed39ff20ca1eb45775bb95ea2d2c
Maxime <maxime.marechalmccoy@gmail.com>
2023-10-20 10:00:09 -0700

    added status logic in model and show on table

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,3 +1,12 @@
 class Sale < ApplicationRecord
-  
+  def finished?
+    # self is not needed, it's implicit
+    self.ends_on < Date.current
+  end
+  def upcoming? ()
+    self.starts_on > Date.current
+  end
+  def active? ()
+    !upcoming? && !finished?
+  end
 end
