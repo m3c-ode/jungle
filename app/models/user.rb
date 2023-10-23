1f6fbac0d660d3f7c171d3ab523364651a88b5fd
Maxime <maxime.marechalmccoy@gmail.com>
2023-10-23 05:45:41 -0700

    defined first tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,26 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :first_name
-  validates :last_name
-  validates :email
-  validates :password
-  validates :password_confirmation
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 4 }
+  validates :password_confirmation, presence: true
 
+  # validate :check_user
+
+  # private
+  # def check_user ()
+  #   puts "Inside validation"
+  #   p self
+  # end
+
+  def self.authenticate_with_credentials(email, password)
+    return unless !email.nil? && !password.nil?
+
+    user = User.find_by_email(email.strip.downcase)
+    return unless user && user.authenticate(password)
+
+    user
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,20 +3,113 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 
-  describe "Validations"  do
-    it "email, first name, last name, " do
-      
+  describe 'Validations' do
+    it 'should validate if all fields are provided' do
+      user = User.create(
+        first_name: 'Bob',
+        last_name: 'Morane',
+        email: 'bob@morane.ca',
+        password: '1111',
+        password_confirmation: '1111'
+      )
+      # puts "Checking user"
+      # p user
+      # print 'user valid?: '
+      # p user.valid?
+      expect(user).to be_valid
+    end
+    it 'should error if missing field' do
+      user = User.create(
+        first_name: 'Bob',
+        # last_name: nil,
+        email: 'bob@morane.ca',
+        password: '1111',
+        password_confirmation: '1111'
+      )
+      # puts 'Checking user'
+      # p user
+      # print 'user valid?: '
+      # p user.valid?
+      # p user.errors.full_messages
+      expect(user).to be_invalid
+      expect(user.errors.full_messages[0]).to include "can't be blank"
     end
 
-    it "password and password_confirmation should be identical" do
-      
+    it 'password and password_confirmation should be identical' do
+      user = User.new(
+        first_name: 'Bob',
+        last_name: 'Morane',
+        email: 'bob@morane.ca',
+        password: '1111',
+        password_confirmation: '2222'
+      )
+      # puts 'Checking password identical'
+      # p user
+      # print 'user valid?: '
+      # p user.valid?
+      # p user.errors.full_messages
+      expect(user).to be_invalid
+      expect(user.errors.full_messages[0]).to include "confirmation doesn't match"
     end
-    it "invalidates if password and password_confirmation are different" do
-      
+    it 'password should be at least 4 characters' do
+      user = User.new(
+        first_name: 'Bob',
+        last_name: 'Morane',
+        email: 'bob@morane.ca',
+        password: '111',
+        password_confirmation: '111'
+      )
+      puts 'Checking password length'
+      p user
+      print 'user valid?: '
+      p user.valid?
+      p user.errors.full_messages
+      expect(user).to be_invalid
+      expect(user.errors.full_messages[0]).to include 'Password is too short'
     end
-    it "emails should be unique" do
-      
+    it 'password fields should be provided' do
+      user = User.create(
+        first_name: 'Bob',
+        last_name: 'Morane',
+        email: 'bob@morane.ca',
+        password: '1111',
+        password_confirmation: nil
+      )
+      # puts 'Checking password provided'
+      # p user
+      # print 'user valid?: '
+      # p user.valid?
+      # p user.errors.full_messages
+      expect(user).to be_invalid
+      expect(user.errors.full_messages[0]).to include "can't be blank"
+    end
+    it 'emails should be unique' do
+      user1 = User.create(
+        first_name: 'Bob',
+        last_name: 'Morane',
+        email: 'bob@morane.ca',
+        password: '1111',
+        password_confirmation: '1111'
+      )
+      # puts 'Checking password uniqueness'
+      # p user
+      # print 'user valid?: '
+      # p user1.valid?
+      # p user1.errors.full_messages
+      user2 = User.create(
+        first_name: 'Bob',
+        last_name: 'Morane',
+        email: 'BOB@morane.ca',
+        password: '1111',
+        password_confirmation: '1111'
+      )
+      # puts 'Checking password uniqueness'
+      # p user2
+      # print 'user valid?: '
+      # p user2.valid?
+      # p user2.errors.full_messages
+      expect(user2).to be_invalid
+      expect(user2.errors.full_messages[0]).to include 'Email has already been taken'
     end
   end
-
 end
