132596d730bc82a6a544a89b070f44862b10f2b5
Maxime <maxime.marechalmccoy@gmail.com>
2023-10-24 15:33:08 -0700

    added admin::basecontroller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,4 @@
-class Admin::DashboardController < ApplicationController
-  # http_basic_authenticate_with name: ENV["ADMIN_USR"], password: ENV["ADMIN_PWD"]
+class Admin::DashboardController < Admin::BaseController
 
   def show
     @products = Product.all
--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -1,4 +1,4 @@
-class Admin::SalesController < ApplicationController
+class Admin::SalesController < Admin::BaseController
   def index
     # render :index
      @sales = Sale.all
